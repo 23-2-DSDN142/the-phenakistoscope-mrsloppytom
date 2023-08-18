9c0181e769879e2f4a9622d57549c27045c3cc36
mrsloppytom <140354003+mrsloppytom@users.noreply.github.com>
2023-08-18 16:13:45 +1200

    first sync

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -3,22 +3,34 @@ const SLICE_COUNT = 10;
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image("ball" , "png");
 }
+
+
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 180);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
+  var layer1 = new PLayer(ball);
+  layer1.mode( SWIRL(10) );
   layer1.set_boundary( 200, 1000 );
 
   var layer2 = new PLayer(squares);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
+
+}
+
+
+function ball(x, y, animation, pScope){
+  scale(animation.frame*2);
+
+  scale(0.3)
+  pScope.draw_image("ball",x,y);
 }
 
 function faces(x, y, animation, pScope){
@@ -40,10 +52,14 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
+  fill(255)
+  noStroke()
   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+  fill(100)
+  noStroke()
+ circle(-10,-300-animation.wave()*10,100) // .wave is a cosine wave btw
 
+
+  
 }
