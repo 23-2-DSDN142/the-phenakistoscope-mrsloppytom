34b46e8b1a2a7eba511356146ba9bb360c747058
mrsloppytom <140354003+mrsloppytom@users.noreply.github.com>
2023-08-29 19:08:25 +1200

    added camera lens to middle, test for the celluloid film ring

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -7,6 +7,8 @@ function setup_pScope(pScope){
   pScope.set_direction(CW);
   pScope.set_slice_count(SLICE_COUNT);
   pScope.load_image("ball" , "png");
+  pScope.load_image("camera_lens" , "png");
+  pScope.load_image("film_reel2" , "png");
   pScope.load_image_sequence("horseRun" , "png", 11);
 }
 
@@ -16,64 +18,88 @@ function setup_layers(pScope){
 
   new PLayer(null, 180);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(ball);
-  layer1.mode( SWIRL(10) );
-  layer1.set_boundary( 200, 1000 );
+  // var layer1 = new PLayer(ball);
+  // layer1.mode( SWIRL(10) );
+  // layer1.set_boundary( 200, 1000 );
 
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  // var layer2 = new PLayer(squares);
+  // layer2.mode( RING );
+  // layer2.set_boundary( 0, 400 );
 
   var horseSeq = new PLayer(horse);
   horseSeq.mode(RING);
   horseSeq.set_boundary(800, 1000);
 
+  var film = new PLayer(filmReel);
+  film.mode(RING);
+  film.set_boundary(400, 800);
+
+  var lens = new PLayer(camLens);
+  lens.mode(RING);
+  lens.set_boundary(0, 200);
+
 }
+
+function camLens(x,y,animation, pScope){
+  scale(5)
+  pScope.draw_image("camera_lens",x,y);
+}
+
 
 function horse(x, y, animation, pScope){
 
   // scale(animation.frame*2);
   scale(1.2)
-  pScope.draw_image_from_sequence("horseRun", -10, -750,animation.frame,100);
-
+  pScope.draw_image_from_sequence("horseRun", -10, -720, animation.frame, 100);
 
 }
 
 
-function ball(x, y, animation, pScope){
-  scale(animation.frame*2);
-
-  scale(0.3)
-  pScope.draw_image("ball",x,y);
-}
-
-// function faces(x, y, animation, pScope){
-  
+// function ball(x, y, animation, pScope){
 //   scale(animation.frame*2);
 
-//   ellipse(0,0,50,50); // draw head
-//   fill(30);
-//   ellipse(-10,-10,10,10); //draw eye
-//   ellipse(10,-10,10,10); // draw eye
-//   arc(0,10,20,10,0,180); // draw mouth
+//   scale(0.3)
+//   pScope.draw_image("ball",x,y);
+// }
+
+
+
+
+
+// function squares(x, y, animation, pScope){
+
+//   // this is how you set up a background for a specific layer
+//   let angleOffset = (360 / SLICE_COUNT) / 2
+//   let backgroundArcStart = 270 - angleOffset;
+//   let backgroundArcEnd = 270 + angleOffset;
+
+//   fill(255)
+//   noStroke()
+//   arc(x,50,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+
+//   fill(100)
+//   noStroke()
+//  circle(-10,-300-animation.wave()*10, 100) // .wave is a cosine wave btw
 
 // }
 
-function squares(x, y, animation, pScope){
+function filmReel(x, y, animation, pScope){
 
-  // this is how you set up a background for a specific layer
+  push();
+  scale(2);
+  pScope.draw_image("film_reel2",x,y);
+  pop();
+
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(255)
+  fill(0)
   noStroke()
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  arc(x, y,800,800,backgroundArcStart,backgroundArcEnd);
 
-  fill(100)
-  noStroke()
- circle(-10,-300-animation.wave()*10,100) // .wave is a cosine wave btw
-
-
+ 
   
-}
+  }
+
+
